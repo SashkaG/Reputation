8e863714aa591324b4082a32c644da96e538a1ef
SashkaG <4807091256@mail.ru>
2017-07-06 22:16:53 +0300

    Update Reputation.py

--- Reputation.py
+++ Reputation.py
@@ -25,11 +25,25 @@ def down(e):
     mfirebase.post("/Reputation/" + str(e.id), str(int(b) - 1))
     if a != None:
         mfirebase.delete("/Reputation/" + str(e.id), str(list(a.keys())[0]))
-
+@client.event
+async def on_reaction_remove(reaction, user):
+    mname = user.id
+    e = reaction.message.mentions[0]
+    if reaction.message.channel.name != "tipstrucksnews":
+        return
+    if reaction.emoji == "👍":
+        if mname != e.id:
+            down(e)
+    elif reaction.emoji == "👎":
+        if mname != e.id:
+            up(e)
+    print(reaction.emoji)
 @client.event
 async def on_reaction_add(reaction, user):
     mname = user.id
-    e = reaction.message.author
+    e = reaction.message.mentions[0]
+    if reaction.message.channel.name != "tipstrucksnews":
+        return
     if reaction.emoji=="👍":
         if mname != e.id:
             up(e)
@@ -58,7 +72,7 @@ async def on_message(message):
             rep[e.name]=b
         fsend = [k+" : "+str(v) for k , v in rep.items()]
         fsend.sort(key = lambda e:int(e.split(" : ")[1]))
-        fsend = fsend[10:0:-1]
+        fsend = fsend[11:0:-1]
         await client.send_message(message.channel,"Top:\n"+"\n".join(fsend))
         print("Done!!!")
 @client.event
