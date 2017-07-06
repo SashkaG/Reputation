66ef466987bfad124a4c9348fe078ee1860d31c4
SashkaG <4807091256@mail.ru>
2017-07-06 21:49:02 +0300

    Update Reputation.py

--- Reputation.py
+++ Reputation.py
@@ -9,28 +9,46 @@ async def on_member_join(member):
     server = member.server
     fmt = 'Welcome {0.mention} to {1.name}!'
     await client.send_message(server, fmt.format(member, server))
+
+def up(e):
+    print("saving...")
+    a = mfirebase.get("/Reputation", str(e.id))
+    b = 0 if a == None else str(list(a.values())[0])
+    mfirebase.post("/Reputation/" + str(e.id), str(int(b) + 1))
+    if a != None:
+        mfirebase.delete("/Reputation/" + str(e.id), str(list(a.keys())[0]))
+
+def down(e):
+    print("saving...")
+    a = mfirebase.get("/Reputation", str(e.id))
+    b = 0 if a == None else str(list(a.values())[0])
+    mfirebase.post("/Reputation/" + str(e.id), str(int(b) - 1))
+    if a != None:
+        mfirebase.delete("/Reputation/" + str(e.id), str(list(a.keys())[0]))
+
+@client.event
+async def on_reaction_add(reaction, user):
+    mname = user.id
+    e = reaction.message.author
+    if reaction.emoji=="👍":
+        if mname != e.id:
+            up(e)
+    elif reaction.emoji=="👎":
+        if mname != e.id:
+            down(e)
+    print(reaction.emoji)
 @client.event
 async def on_message(message):
     mname=message.author.id
     if message.content.startswith("/up"):
         for e in message.mentions:
             if e.id!=mname:
-                print("saving...")
-                a = mfirebase.get("/Reputation",str(e.id))
-                b = 0 if a==None else str(list(a.values())[0])
-                mfirebase.post("/Reputation/"+str(e.id),str(int(b)+1))
-                if a != None:
-                    mfirebase.delete("/Reputation/"+str(e.id),str(list(a.keys())[0]))
+                up(e)
         print("Done!")
     if message.content.startswith("/down"):
         for e in message.mentions:
             if e.id!=mname:
-                print("saving...")
-                a = mfirebase.get("/Reputation",str(e.id))
-                b = 0 if a==None else str(list(a.values())[0])
-                mfirebase.post("/Reputation/"+str(e.id),str(int(b)-1))
-                if a!=None:
-                    mfirebase.delete("/Reputation/"+str(e.id),str(list(a.keys())[0]))
+                down(e)
         print("Done!")
     if message.content.startswith("/top"):
         rep = {}
@@ -42,7 +60,7 @@ async def on_message(message):
         fsend.sort(key = lambda e:int(e.split(" : ")[1]))
         fsend = fsend[10:0:-1]
         await client.send_message(message.channel,"Top:\n"+"\n".join(fsend))
-        print("Done!")
+        print("Done!!!")
 @client.event
 async def on_ready():
     print('Logged in as')
